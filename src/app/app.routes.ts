2fe6b2c313cc27d8477463c602483a97363be568
Fazley <22FTT1494@student.pb.edu.bn>
2024-05-14 22:16:17 +0800

    chore: Update home page component imports and remove unused styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
-import { RouterModule, Routes } from '@angular/router';
-import { HomePage } from './home/home.page'; // Adjust the path to the actual location of home.page.ts
+import { Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomePage },
+export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
-  // Add other routes here
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
-
-export const appRoutes = RouterModule.forRoot(routes);
