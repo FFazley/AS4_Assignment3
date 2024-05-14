c8137e9709dc8bc7cbeb46b71e22268225262b4c
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-05-15 00:06:07 +0800

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,21 +9,25 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
-  },  {
+  },
+  {
     path: 'signup',
-    loadComponent: () => import('./signup/signup.page').then( m => m.SignupPage)
+    loadComponent: () =>
+      import('./signup/signup.page').then((m) => m.SignupPage),
   },
   {
     path: 'profilepage',
-    loadComponent: () => import('./profilepage/profilepage.page').then( m => m.ProfilepagePage)
+    loadComponent: () =>
+      import('./profilepage/profilepage.page').then((m) => m.ProfilepagePage),
   },
   {
     path: 'settings',
-    loadComponent: () => import('./settings/settings.page').then( m => m.SettingsPage)
+    loadComponent: () =>
+      import('./settings/settings.page').then((m) => m.SettingsPage),
   },
   {
     path: 'loginpage',
-    loadComponent: () => import('./loginpage/loginpage.page').then( m => m.LoginpagePage)
+    loadComponent: () =>
+      import('./loginpage/loginpage.page').then((m) => m.LoginpagePage),
   },
-
 ];
