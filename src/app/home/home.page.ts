985d5423ce2c4c4b2fc01a36ec6b212a0557df95
Fazley <22FTT1494@student.pb.edu.bn>
2024-05-15 00:13:03 +0800

    refactor: Add center alignment to product image in home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,26 +1,80 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonButtons,IonBadge,IonMenuButton,IonIcon, IonButton, IonItem,IonFooter,IonLabel,IonCard,IonList,IonRow,IonCol,IonGrid } from '@ionic/angular/standalone';
+import {
+  IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBadge, IonMenuButton, IonIcon,
+  IonButton, IonItem, IonFooter, IonLabel, IonCard, IonList, IonRow, IonCol, IonGrid
+} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonItem, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBadge,IonMenuButton,IonIcon,IonFooter,IonLabel,IonCard,IonList,IonRow,IonCol,IonGrid]
+  imports: [
+    IonItem, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+    IonButtons, IonBadge, IonMenuButton, IonIcon, IonFooter, IonLabel, IonCard, IonList, IonRow, IonCol, IonGrid
+  ]
 })
 export class HomePage implements OnInit {
-openProductDetails(arg0: any,arg1: string) {
-throw new Error('Method not implemented.');
-}
-productOfTheDay: any;
-product_Popular: any;
-Product_Trending: any;
+  productOfTheDay: any;
+  product_Popular: any;
+  Product_Trending: any;
 
-  constructor() { }
+  constructor(private router: Router) { 
+    this.productOfTheDay = [{
+      image: 'assets/img/food1.jpg',
+      name: 'Product Name',
+      price: 29.99,
+      rating: 5
+    }];
 
-  ngOnInit() {
+    this.product_Popular = [
+      {
+      image: 'assets/img/food1.jpg',
+        name: 'Popular Product 1',
+        price: 19.99,
+        rating: 4
+      },
+      {
+        image: 'assets/img/food2.jpg',
+        name: 'Popular Product 2',
+        price: 24.99,
+        rating: 3
+      }
+    ];
+
+    this.Product_Trending = [
+      {
+        image: 'assets/img/food3.jpg',
+
+        name: 'Trending Product 1',
+        price: 34.99,
+        rating: 5
+      },
+      {
+        image: 'assets/img/food4.jpg',
+
+        name: 'Trending Product 2',
+        price: 44.99,
+        rating: 4
+      }
+    ];
   }
 
+  ngOnInit() {
+    // You can fetch the product data here from an API or service
+  }
+
+  openProductDetails(product: any, category: string) {
+    // Handle navigation to product details page
+    // Pass necessary data using query params or a state management solution
+    this.router.navigate(['/product-details'], {
+      queryParams: {
+        product: JSON.stringify(product),
+        category: category
+      }
+    });
+  }
 }
