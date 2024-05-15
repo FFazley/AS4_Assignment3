4f460aa63f5309bc80d5710cc98cbadc39bec942
Fazley <22FTT1494@student.pb.edu.bn>
2024-05-15 22:30:08 +0800

    refactor: Update home page HTML layout and styling

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,19 +3,20 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
   IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBadge, IonMenuButton, IonIcon,
-  IonButton, IonItem, IonFooter, IonLabel, IonCard, IonList, IonRow, IonCol, IonGrid, IonMenu
-} from '@ionic/angular/standalone';
+  IonButton, IonItem, IonFooter, IonLabel, IonCard, IonList, IonRow, IonCol, IonGrid, IonMenu, IonSearchbar, IonCardTitle, IonCardHeader, IonCardContent, IonCardSubtitle, IonApp} from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { homeOutline, heartOutline, cartOutline ,cart} from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [
+  imports: [IonApp, IonCardSubtitle, IonCardContent, IonCardHeader, IonCardTitle, IonSearchbar, 
     IonItem, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
-    IonButtons, IonBadge, IonMenuButton, IonIcon, IonFooter, IonLabel, IonCard, IonList, IonRow, IonCol, IonGrid, IonMenu
-  ]
+    IonButtons, IonBadge, IonMenuButton, IonIcon, IonFooter, IonLabel, IonCard, IonList, IonRow, IonCol, IonGrid, IonMenu,IonCardTitle,IonCardHeader,IonCardContent,IonCardSubtitle,IonBadge], 
+  
 })
 export class HomePage implements OnInit {
   productOfTheDay: any;
@@ -59,7 +60,16 @@ export class HomePage implements OnInit {
         rating: 4
       }
     ];
-  }
+  
+  
+    addIcons({
+      'home-outline': homeOutline,
+      'heart-outline': heartOutline,
+      'cart-outline': cartOutline,
+      'cart': cart
+    });
+  
+  };
 
   ngOnInit() {
     // You can fetch the product data here from an API or service
