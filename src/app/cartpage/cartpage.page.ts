4a842d90a1429f8ec00ddae41bd064080b196f05
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-05-15 22:13:10 +0800

    cart page modified

--- src/app/cartpage/cartpage.page.ts
+++ src/app/cartpage/cartpage.page.ts
@@ -1,20 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonIcon,
+  IonCard,
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonItem,
+  IonList,
+  IonLabel,
+  IonText,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonBackButton,
+  IonButtons,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-cartpage',
   templateUrl: './cartpage.page.html',
   styleUrls: ['./cartpage.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonButtons,
+    IonBackButton,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardHeader,
+    IonText,
+    IonLabel,
+    IonList,
+    IonItem,
+    IonImg,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonCardContent,
+    IonCard,
+    IonIcon,
+    IonButton,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class CartpagePage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
